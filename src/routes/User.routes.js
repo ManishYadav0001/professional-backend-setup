5c31f7dac9d725a917da747ba60ca0e988a394fb
Manish yadav <krustermanish@gmail.com>
2025-08-15 08:18:45 +0530

    added more user routes

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/registerUser.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logOutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverimage } from "../controllers/registerUser.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,6 +25,17 @@ router.route("/logout").post(
 router.route("/refresh-Token").post(
   refreshAccessToken
 )
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword)
 
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-details").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverimage").patch(verifyJWT, upload.single("coverimage"), updateUserCoverimage)
+
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
